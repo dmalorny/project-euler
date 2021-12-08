9a444689d13603cca9e00571bf509052ea5287b3
Dirk Malorny <dirk@malorny.net>
2021-12-08 17:33:29 +0100

    problem 5

---
+++ 0005/problem5.swift
@@ -0,0 +1,21 @@
+func problem5(max: Int) {
+    var num = max
+    var found = false
+    while (!found) {
+        var dividable = true
+        for divisor in 2 ... max {
+            if (num % divisor != 0) {
+                dividable = false
+                break    
+            }
+        }
+        if (dividable) {
+            found = true
+        } else {
+            num += max
+        }
+    }
+    print("result: \(num)")
+}
+
+problem5(max: 20)
