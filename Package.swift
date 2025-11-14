30e490ab51f5f4eca5d61687e392c360ebbc8ade
Dirk Malorny <dirk@malorny.net>
2025-11-14 10:34:38 +0100

    Updated dependencies; added test plan

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "ProjectEuler",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.7.0"),
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.2.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
